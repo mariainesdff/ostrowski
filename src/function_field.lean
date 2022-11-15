7b8a1f7dc61a6629a07f82c5825114040bf16b28
Biiiilly <bm221@ic.ac.uk>
2022-11-15 12:38:25 +0000

    add proof for function field

--- src/function_field.lean
+++ src/function_field.lean
@@ -109,7 +109,7 @@ begin
       simp only [h₁, h₂, zero_add, if_false] },
     { have h₂ : ¬ s = 0 := sorry, -- this is false,
       simp only [h₁, h₂, if_false],
-      
+
       sorry} }
 end
 
@@ -119,9 +119,11 @@ end infty
 def polynomial.maximal_ideal_of_irreducible {K : Type*} [field K] [decidable_eq (ratfunc K)]
   {p : polynomial K} (hp : irreducible p) : is_dedekind_domain.height_one_spectrum (K[X]) :=
 { as_ideal := ideal.span({p}),
-  is_prime := sorry,
-  ne_bot   := sorry }
-
+  is_prime := (ideal.span_singleton_prime (prime.ne_zero 
+    (principal_ideal_ring.irreducible_iff_prime.mp hp))).mpr 
+      (principal_ideal_ring.irreducible_iff_prime.mp hp),
+  ne_bot   := by simp only [ne.def, ideal.span_singleton_eq_bot, 
+    prime.ne_zero (principal_ideal_ring.irreducible_iff_prime.mp hp), not_false_iff] }
 
 section adic
 
@@ -132,20 +134,89 @@ def ring_norm.adic {K : Type*} [field K] [decidable_eq (ratfunc K)] (c : ℝ) (h
 { to_fun    := λ r, if hr : r = 0 then 0 else c ^ multiplicative.to_add (with_zero.unzero
   ((@polynomial.maximal_ideal_of_irreducible K _ _ p hp).valuation.ne_zero_iff.mpr hr)),
   map_zero' := sorry,
-  add_le'   := sorry,
+  add_le'   := 
+  begin
+    intros r s,
+    by_cases r + s = 0,
+    { simp only [h, dif_pos],
+      by_cases hr : r = 0,
+      { have hs : s = 0,
+        {sorry},
+        simp only [hs, hr, dif_pos],
+        linarith },
+      { have hs : ¬ s = 0,
+        {sorry},
+        simp only [hs, hr, not_false_iff, dif_neg],
+        exact add_nonneg (zpow_nonneg (le_of_lt hc_pos) _) 
+          (zpow_nonneg (le_of_lt hc_pos) _) } },
+    { simp only [h, not_false_iff, dif_neg],
+      by_cases hr : r = 0,
+      { have hs : ¬ s = 0,
+        {sorry},
+        simp only [hr, hs, not_false_iff, zero_add, dif_pos, dif_neg] },
+      { by_cases hs : s = 0,
+        {sorry},
+        {sorry} } }
+  end,
   neg'      := sorry,
-  eq_zero_of_map_eq_zero' := sorry,
+  eq_zero_of_map_eq_zero' := 
+  begin
+    intros x hx,
+    by_contra,
+    simp only [h, not_false_iff, dif_neg] at hx,
+    linarith [zpow_eq_zero hx]
+  end,
   mul_le'   := sorry }
+
+@[simp] lemma ring_norm.adic_def {K : Type*} [field K] [decidable_eq (ratfunc K)] 
+  (c : ℝ) (hc_pos : 0 < c) (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) 
+    (r : ratfunc K): ring_norm.adic c hc_pos hc_one_lt hp r = if hr : r = 0 then 0 else 
+      c ^ multiplicative.to_add (with_zero.unzero ((
+        @polynomial.maximal_ideal_of_irreducible K _ _ p hp).valuation.ne_zero_iff.mpr hr)) 
+:= rfl
 
 lemma ring_norm.adic_mul_eq (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
   (hc_pos : 0 < c) (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) :
   mul_eq (@ring_norm.adic K _ _ c hc_pos hc_one_lt p hp) :=
-sorry
+begin
+  intros x y,
+  simp only [mul_eq_zero, valuation.map_mul, ring_norm.adic_def],
+  by_cases (x = 0 ∨ y = 0),
+  {sorry},
+  { rw not_or_distrib at h,
+    simp only [h.1, h.2, or_self, not_false_iff, dif_neg],
+    rw ←@zpow_add₀ _ _ c (by linarith),
+    congr,
+    rw ←to_add_mul,
+    congr,
+    rw ←with_zero.coe_inj,
+    push_cast,
+    repeat {rw with_zero.coe_unzero} }
+end
 
 lemma ring_norm.adic_is_nonarchimedean (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
   (hc_pos : 0 < c) (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) :
   is_nonarchimedean (@ring_norm.adic K _ _ c hc_pos hc_one_lt p hp) :=
-sorry
+begin
+  intros x y,
+  simp only [nonempty_of_inhabited, ring_norm.adic_def, le_max_iff],
+  by_cases x + y = 0,
+  {sorry},
+  { simp only [h, not_false_iff, dif_neg],
+    by_cases hx : x = 0,
+    { have hy : ¬ y = 0,
+      {sorry},
+      simp only [hx, hy, not_false_iff, zero_add, dif_neg, le_refl, or_true] },
+    { left,
+      simp only [hx, not_false_iff, dif_neg],
+      repeat {rw ←real.rpow_int_cast},
+      apply real.rpow_le_rpow_of_exponent_le,
+      { linarith },
+      { norm_cast,
+        simp only [multiplicative.to_add_le],
+        
+        sorry} } }
+end
 
 end adic
 
