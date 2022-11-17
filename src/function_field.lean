03551fd89f93e2106660d6abaf9aecbb2b515a3d
Biiiilly <bm221@ic.ac.uk>
2022-11-17 18:32:22 +0000

    change ring_norm to mul_ring_norm

--- src/function_field.lean
+++ src/function_field.lean
@@ -24,12 +24,12 @@ open_locale polynomial
 section infty
 
 /-- The norm on K(X) associated to the place at infinity. -/
-def ring_norm.infty (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ) (hc_pos : 0 < c)
-  (hc_one_lt : 1 < c) : ring_norm (ratfunc K) :=
-{ to_fun    := λ r, if r = 0 then 0 else c ^ r.int_degree,
+noncomputable! def mul_ring_norm.infty (K : Type*) [field K] [decidable_eq (ratfunc K)] 
+  (c : ℝ) (hc_pos : 0 < c) (hc_one_lt : 1 < c) : mul_ring_norm (ratfunc K) :=
+{ to_fun := λ r, if r = 0 then 0 else c ^ r.int_degree,
   map_zero' := sorry,
-  add_le'   := sorry,
-  neg'      := λ r,
+  add_le' := sorry,
+  neg' := λ r,
   begin
     by_cases r = 0,
     { simp only [h, neg_zero] } ,
@@ -39,63 +39,42 @@ def ring_norm.infty (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ) (
         exact neg_eq_zero.1 h₁ },
       simp only [h, h₁, ratfunc.int_degree_neg] }
   end,
+  map_one' := sorry,
+  map_mul' :=
+  begin
+    intros r s,
+    simp [mul_eq_zero, ite_mul, mul_ite, mul_zero, zero_mul],
+    by_cases r = 0 ∨ s = 0,
+    { simp only [h, if_true],
+      cases h,
+      { simp only [h, eq_self_iff_true, if_true, if_t_t] },
+      { simp only [h, eq_self_iff_true, if_true] } },
+    { simp only [h, if_false],
+      rw not_or_distrib at h,
+      cases h with h₁ h₂,
+      simp only [h₁, h₂, if_false],
+      rw ratfunc.int_degree_mul h₁ h₂,
+      repeat {rw ←real.rpow_int_cast},
+      push_cast,
+      exact real.rpow_add hc_pos _ _ }
+  end,
   eq_zero_of_map_eq_zero' := λ x hx,
   begin
     by_contra,
     simp only [h, if_false] at hx,
     linarith [zpow_eq_zero hx],
-  end,
-  mul_le'   := λ x y,
-  begin
-    by_cases x * y = 0,
-    { simp only [h, eq_self_iff_true, if_true, ite_mul, mul_ite, mul_zero, zero_mul],
-      rw mul_eq_zero at h,
-      cases h,
-      { simp only [h, eq_self_iff_true, if_true, if_t_t] },
-      { simp only [h, eq_self_iff_true, if_true] } },
-    { simp only [h, if_false, ite_mul, mul_ite, mul_zero, zero_mul],
-      rw mul_eq_zero at h,
-      rw not_or_distrib at h,
-      cases h with h₁ h₂,
-      simp only [h₁, h₂, if_false],
-      rw ratfunc.int_degree_mul h₁ h₂,
-      apply eq.le,
-      repeat {rw ←real.rpow_int_cast},
-      push_cast,
-      exact real.rpow_add hc_pos _ _ }
   end }
 
-@[simp] lemma ring_norm.infty_def (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ) 
+@[simp] lemma mul_ring_norm.infty_def (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ) 
   (hc_pos : 0 < c) (hc_one_lt : 1 < c) (r : ratfunc K):
-    ring_norm.infty K c hc_pos hc_one_lt r = if r = 0 then 0 else c ^ r.int_degree := rfl
+    mul_ring_norm.infty K c hc_pos hc_one_lt r = if r = 0 then 0 else c ^ r.int_degree := rfl
 
-lemma ring_norm.infty_mul_eq (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
+lemma mul_ring_norm.infty_is_nonarchimedean (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
   (hc_pos : 0 < c) (hc_one_lt : 1 < c) :
-  mul_eq (ring_norm.infty K c hc_pos hc_one_lt) :=
+  is_nonarchimedean (mul_ring_norm.infty K c hc_pos hc_one_lt) :=
 begin
   intros r s,
-  simp only [ring_norm.infty_def, mul_eq_zero, ite_mul, mul_ite, mul_zero, zero_mul],
-  by_cases r = 0 ∨ s = 0,
-  { simp only [h, if_true],
-    cases h,
-    { simp only [h, eq_self_iff_true, if_true, if_t_t] },
-    { simp only [h, eq_self_iff_true, if_true] } },
-  { simp only [h, if_false],
-    rw not_or_distrib at h,
-    cases h with h₁ h₂,
-    simp only [h₁, h₂, if_false],
-    rw ratfunc.int_degree_mul h₁ h₂,
-    repeat {rw ←real.rpow_int_cast},
-    push_cast,
-    exact real.rpow_add hc_pos _ _ }
-end
-
-lemma ring_norm.infty_is_nonarchimedean (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
-  (hc_pos : 0 < c) (hc_one_lt : 1 < c) :
-  is_nonarchimedean (ring_norm.infty K c hc_pos hc_one_lt) :=
-begin
-  intros r s,
-  simp only [ring_norm.infty_def, le_max_iff],
+  simp only [mul_ring_norm.infty_def, le_max_iff],
   by_cases r + s = 0,
   { simp only [h, eq_self_iff_true, if_true],
     sorry},
@@ -128,13 +107,13 @@ def polynomial.maximal_ideal_of_irreducible {K : Type*} [field K] [decidable_eq 
 section adic
 
 /-- The norm on K(X) associated to an irreducible polynomial. -/
-def ring_norm.adic {K : Type*} [field K] [decidable_eq (ratfunc K)] (c : ℝ) (hc_pos : 0 < c)
-  (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) :
-  ring_norm (ratfunc K) :=
-{ to_fun    := λ r, if hr : r = 0 then 0 else c ^ multiplicative.to_add (with_zero.unzero
+noncomputable! def mul_ring_norm.adic {K : Type*} [field K] [decidable_eq (ratfunc K)] (c : ℝ) 
+  (hc_pos : 0 < c) (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) :
+    mul_ring_norm (ratfunc K) :=
+{ to_fun := λ r, if hr : r = 0 then 0 else c ^ multiplicative.to_add (with_zero.unzero
   ((@polynomial.maximal_ideal_of_irreducible K _ _ p hp).valuation.ne_zero_iff.mpr hr)),
   map_zero' := sorry,
-  add_le'   := 
+  add_le' := 
   begin
     intros r s,
     by_cases r + s = 0,
@@ -158,48 +137,45 @@ def ring_norm.adic {K : Type*} [field K] [decidable_eq (ratfunc K)] (c : ℝ) (h
         {sorry},
         {sorry} } }
   end,
-  neg'      := sorry,
-  eq_zero_of_map_eq_zero' := 
+  neg' := sorry,
+  map_one' := sorry,
+  map_mul' :=
+  begin
+    intros x y,
+    simp [mul_eq_zero, valuation.map_mul],
+    by_cases (x = 0 ∨ y = 0),
+    {sorry},
+    { rw not_or_distrib at h,
+      simp only [h.1, h.2, or_self, not_false_iff, dif_neg],
+      rw ←@zpow_add₀ _ _ c (by linarith),
+      congr,
+      rw ←to_add_mul,
+      congr,
+      rw ←with_zero.coe_inj,
+      push_cast,
+      repeat {rw with_zero.coe_unzero} },
+  end,
+  eq_zero_of_map_eq_zero' :=
   begin
     intros x hx,
     by_contra,
     simp only [h, not_false_iff, dif_neg] at hx,
     linarith [zpow_eq_zero hx]
-  end,
-  mul_le'   := sorry }
+  end }
 
-@[simp] lemma ring_norm.adic_def {K : Type*} [field K] [decidable_eq (ratfunc K)] 
+@[simp] lemma mul_ring_norm.adic_def {K : Type*} [field K] [decidable_eq (ratfunc K)] 
   (c : ℝ) (hc_pos : 0 < c) (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) 
-    (r : ratfunc K): ring_norm.adic c hc_pos hc_one_lt hp r = if hr : r = 0 then 0 else 
+    (r : ratfunc K): mul_ring_norm.adic c hc_pos hc_one_lt hp r = if hr : r = 0 then 0 else 
       c ^ multiplicative.to_add (with_zero.unzero ((
         @polynomial.maximal_ideal_of_irreducible K _ _ p hp).valuation.ne_zero_iff.mpr hr)) 
 := rfl
 
-lemma ring_norm.adic_mul_eq (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
+lemma mul_ring_norm.adic_is_nonarchimedean (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
   (hc_pos : 0 < c) (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) :
-  mul_eq (@ring_norm.adic K _ _ c hc_pos hc_one_lt p hp) :=
+  is_nonarchimedean (@mul_ring_norm.adic K _ _ c hc_pos hc_one_lt p hp) :=
 begin
   intros x y,
-  simp only [mul_eq_zero, valuation.map_mul, ring_norm.adic_def],
-  by_cases (x = 0 ∨ y = 0),
-  {sorry},
-  { rw not_or_distrib at h,
-    simp only [h.1, h.2, or_self, not_false_iff, dif_neg],
-    rw ←@zpow_add₀ _ _ c (by linarith),
-    congr,
-    rw ←to_add_mul,
-    congr,
-    rw ←with_zero.coe_inj,
-    push_cast,
-    repeat {rw with_zero.coe_unzero} }
-end
-
-lemma ring_norm.adic_is_nonarchimedean (K : Type*) [field K] [decidable_eq (ratfunc K)] (c : ℝ)
-  (hc_pos : 0 < c) (hc_one_lt : 1 < c) {p : polynomial K} (hp : irreducible p) :
-  is_nonarchimedean (@ring_norm.adic K _ _ c hc_pos hc_one_lt p hp) :=
-begin
-  intros x y,
-  simp only [nonempty_of_inhabited, ring_norm.adic_def, le_max_iff],
+  simp only [nonempty_of_inhabited, mul_ring_norm.adic_def, le_max_iff],
   by_cases x + y = 0,
   {sorry},
   { simp only [h, not_false_iff, dif_neg],
@@ -220,15 +196,13 @@ end
 
 end adic
 
-/- Ostrowski's Theorem 
+/-- Ostrowski's Theorem -/
 theorem rat_ring_norm_p_adic_or_real (K : Type*) [field K] [decidable_eq (ratfunc K)]
-  (c : ℝ) (hc_pos : 0 < c) (hc_one_lt : 1 < c) (f : ring_norm (ratfunc K))
-  (hf_nontriv : f ≠ 1) (hf_triv_K : ∀ {x : K} (hx : x ≠ 0), f (ratfunc.C x) = 1)
-  (hf_mul : mul_eq f) :
-  (ring_norm.equiv f (ring_norm.infty K c hc_pos hc_one_lt)) ∨
-  ∃ (p : K[X]) [hp : irreducible p],
-    ring_norm.equiv f (@ring_norm.adic K _ _ c hc_pos hc_one_lt p hp) :=
+  (c : ℝ) (hc_pos : 0 < c) (hc_one_lt : 1 < c) (f : mul_ring_norm (ratfunc K))
+    (hf_nontriv : f ≠ 1) (hf_triv_K : ∀ {x : K} (hx : x ≠ 0), f (ratfunc.C x) = 1) :
+      (mul_ring_norm.equiv f (mul_ring_norm.infty K c hc_pos hc_one_lt)) ∨
+        ∃ (p : K[X]) [hp : irreducible p],
+          mul_ring_norm.equiv f (@mul_ring_norm.adic K _ _ c hc_pos hc_one_lt p hp) :=
 begin
   sorry
 end
--/
